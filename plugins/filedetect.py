ddcf14c6a988188121176cf325c6b47b392bb953
Decoderseeman <99487702+Decoderseeman@users.noreply.github.com>
2022-08-01 00:46:47 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -11,12 +11,12 @@ async def refunc(client, message):
        file = msg.reply_to_message
        media = file.media
        await reply_message.delete()
-       button = [[InlineKeyboardButton("📁 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝚂",callback_data = "upload_document")]]
+       button = [[InlineKeyboardButton("📁 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝚂 📁",callback_data = "upload_document")]]
        if str(media) in ["MessageMediaType.VIDEO", "MessageMediaType.DOCUMENT"]:
-           button.append([InlineKeyboardButton("🎥 𝚅𝙸𝙳𝙴𝙾",callback_data = "upload_video")])
+           button.append([InlineKeyboardButton("🎥 𝚅𝙸𝙳𝙴𝙾 🎥",callback_data = "upload_video")])
        elif str(media) == "MessageMediaType.AUDIO":
-           button.append([InlineKeyboardButton("🎵 𝙰𝙾𝚄𝙳𝙸𝙾",callback_data = "upload_audio")])
+           button.append([InlineKeyboardButton("🎵 𝙰𝚄𝙳𝙸𝙾 🎵",callback_data = "upload_audio")])
        await message.reply_text(
-          f"**Select the output file type**\n**Output FileName** :- ```{new_name}```",
+          f"**```{new_name}```**\n\n**𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @MKV_BOTS**",
           reply_to_message_id=file.id,
           reply_markup=InlineKeyboardMarkup(button))
